af8dcf71f446e0cad0f12679a3e5908c495614f9
subhash <subhashpal2020@gmail.com>
2022-04-27 14:31:33 +0530

    data s

--- Untitled-3.cpp
+++ Untitled-3.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+// CREATING NODES
 struct node
 {
 	int data;
@@ -10,7 +10,7 @@ struct node
 
 int main()
 {
-	int a=1;
+	int a=1; //A=1 IS compulsory otherwise code will termonte soon
 	struct node *head=0;
 	struct node *newnode;
 	struct node *temp;
@@ -19,7 +19,7 @@ int main()
 	{
 	
 	printf("enter your data");
-	newnode=(struct node *)malloc(sizeof(struct node));
+	newnode=(struct node *)malloc(sizeof(struct node)); //creating nodes
 	
 	
 	scanf("%d",&newnode->data);
@@ -37,24 +37,23 @@ int main()
 		temp=newnode;
 	}
 		
-	printf("do you want more to enter");
+	printf("do you want more to enter"); // asking user for more or nore
 	scanf("%d",&a);
 	
     }
-    newnode->next=0;
+    newnode->next=0;  // last node->next is 0
     
-    
+
     
     printf("enter data for newnode at start");
-    
     newnode=(struct node *)malloc(sizeof(struct node));
     scanf("%d",&newnode->data);
     temp=head;
     newnode->next=temp;
-    head=newnode;
+    head=newnode;  // adding node in starting delte it if not needed
     
     
-    temp=head;
+    temp=head;   // node travesre 
     while(temp!=0)
     {
     	printf("%d\n",temp->data);
